f1df04fa3150d6f568b0cb2bdfbbca74df7cc03e
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-02-01 12:45:09 +0100

    Added GPL 3.0 licensing headers and explicitly mentioned it in the README.

--- training_quantization/models/gate_classifier_PyTorch_model.py
+++ training_quantization/models/gate_classifier_PyTorch_model.py
@@ -1,9 +1,35 @@
-"""
-Classes for deep learning model used in my master thesis
-Adapted from https://github.com/pulp-platform/pulp-dronet/blob/master/pulp-dronet-v2/model/dronet_v2_gapflow.py
+#
+# Copyright (C) 2022-2024 ETH Zurich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0
+# ======================================================================
+#
+# Authors: 
+# Konstantin Kalenberg, ETH Zurich (kkalenbe@ethz.ch)
+# Hanna Müller ETH Zurich (hanmuell@iis.ee.ethz.ch)
+# Tommaso Polonelli, ETH Zurich (tommaso.polonelli@pbl.ee.ethz.ch)
+# Alberto Schiaffino, ETH Zurich (aschiaffino@ethz.ch)
+# Vlad Niculescu, ETH Zurich (vladn@ethz.ch)
+# Cristian Cioflan, ETH Zurich (cioflanc@ethz.ch)
+# Michele Magno, ETH Zurich (michele.magno@pbl.ee.ethz.ch)
+# Luca Benini, ETH Zurich (lbenini@iis.ee.ethz.ch)
+#
+# Classes for deep learning model used in my master thesis
+# Adapted from https://github.com/pulp-platform/pulp-dronet/blob/master/pulp-dronet-v2/model/dronet_v2_gapflow.py
 
-author: Konstantin Kalenberg
-"""
+
 import torch
 import torch.nn as nn
 
--- training_quantization/training_gate_navigator_TensorFlow.py
+++ training_quantization/training_gate_navigator_TensorFlow.py
@@ -1,8 +1,32 @@
-"""
-This script is used to train the weights of the GateNavigator network
-
-Author: Alberto Schiaffino
-"""
+#
+# Copyright (C) 2022-2024 ETH Zurich
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0
+# ======================================================================
+#
+# Authors: 
+# Konstantin Kalenberg, ETH Zurich (kkalenbe@ethz.ch)
+# Hanna Müller ETH Zurich (hanmuell@iis.ee.ethz.ch)
+# Tommaso Polonelli, ETH Zurich (tommaso.polonelli@pbl.ee.ethz.ch)
+# Alberto Schiaffino, ETH Zurich (aschiaffino@ethz.ch)
+# Vlad Niculescu, ETH Zurich (vladn@ethz.ch)
+# Cristian Cioflan, ETH Zurich (cioflanc@ethz.ch)
+# Michele Magno, ETH Zurich (michele.magno@pbl.ee.ethz.ch)
+# Luca Benini, ETH Zurich (lbenini@iis.ee.ethz.ch)
+#
+# This script provides utility functions for the deep learning process
 
 
 import shutil
